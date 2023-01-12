8469dabaef60a9757d477848918d2bb76f025d99
jwt2706 <jwt2home@gmail.com>
2023-01-12 12:42:57 -0500

    image reel & translations

--- script.js
+++ script.js
@@ -13,30 +13,35 @@ function loadContent(l) {
 }
 
 function shuffleLoad() {
-    img1.setAttribute("style", "transform: rotate(" + 7 + "deg)");
-    img2.setAttribute("style", "transform: rotate(" + -4 + "deg)");
-    //img3.setAttribute("style", "transform: rotate(" + 1 + "deg)");
-    document.getElementById('img1').style.zIndex = 7;
-    document.getElementById('img2').style.zIndex = 6;
-    document.getElementById('img3').style.zIndex = 5;
+    img1.setAttribute("style", "transform: rotate(" + 12 + "deg)");
+    img2.setAttribute("style", "transform: rotate(" + -9 + "deg)");
+    img3.setAttribute("style", "transform: rotate(" + 5 + "deg)");
+    document.getElementById('img1').style.zIndex = 13;
+    document.getElementById('img2').style.zIndex = 12;
+    document.getElementById('img3').style.zIndex = 11;
+    document.getElementById('img4').style.zIndex = 10;
 }
 
 function shuffle() {
+    let numOfImgs = 4;
+    const credits = ["Hadi H.", "Jacob T.", "Jacob T.", "Jacob T."];
     let imgId = 'img' + String(frontImg);
     let shufObj = document.getElementById(imgId);
     document.getElementById("shuffleBtn").disabled = true;
     shufObj.style.animation='flip 2s ease';
-    shufObj.style.zIndex -= 3;
+    shufObj.style.zIndex -= numOfImgs;
     setTimeout(function() {
         shufObj.style.animation='';
         document.getElementById("shuffleBtn").disabled = false;
     }, 2000);
-    if (frontImg < 3) {
+    if (frontImg < numOfImgs) {
         frontImg += 1;
     } else {
         frontImg = 1;
         shuffleLoad();
     }
+    document.getElementById("credits").innerHTML = "Image credit/Crédit d'image: " + credits[frontImg-1]
+    console.log(frontImg);
 }
 
 function typeEventName(txt, i) {
