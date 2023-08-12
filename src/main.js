c107bb37bb4c52b4dffe736089e6716ce3a3841d
jwt2706 <jwt2home@gmail.com>
2023-08-12 17:10:57 -0400

    ga4

--- src/main.js
+++ src/main.js
@@ -1,86 +1,55 @@
 import './style.css';
 import * as THREE from 'three';
-// Setup
 
+// build the scene and attach it to the canvas
 const scene = new THREE.Scene();
-
 const view = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
   alpha: true,
 });
 
+// setup renderer
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-view.position.setZ(30);
-view.position.setX(-3);
-
 renderer.render(scene, view);
 
-// Torus
-
+// torus object
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
 const torus = new THREE.Mesh(geometry, material);
-
 scene.add(torus);
 
-// Lights
-
+// lighting
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
-//const ambientLight = new THREE.AmbientLight(0xffffff);
-//scene.add(pointLight, ambientLight);
-
-
-
-// Background
-
-//const spaceTexture = new THREE.TextureLoader().load('space.jpg');
-//scene.background = spaceTexture;
-
-
-// Avatar
-
+// camera object
 const cameraTexture = new THREE.TextureLoader().load('shutter.png');
-
 const camera = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: cameraTexture }));
-
 scene.add(camera);
 
+// inital positions
 camera.position.z = -5;
 camera.position.x = 2;
 view.position.z = 1;
 view.position.x = 0;
 view.rotation.y = 0;
 
-// Scroll Animation
-
-
-function moveCamera() {
-  const t = 1;
-
+// rotate camera on scroll
+function rotateCamera() {
   camera.rotation.y += 0.05;
   camera.rotation.z += 0.05;
 }
+document.body.onscroll = rotateCamera;
+rotateCamera();
 
-document.body.onscroll = moveCamera;
-moveCamera();
-
-// Animation Loop
-
+// animation loop
 function animate() {
   requestAnimationFrame(animate);
-
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
-
-  // controls.update();
-
   renderer.render(scene, view);
 }
-
 animate();
