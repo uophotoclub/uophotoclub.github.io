fe42f68ed015bcfd4fd4168bbf3d2836b7994fb3
jwt2706 <jwt2home@gmail.com>
2024-07-30 13:09:47 -0400

    cleanup, font, icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import { BackgroundGradientAnimation } from "./components/background-gradient-animation";
+import { FaDiscord } from 'react-icons/fa';
+import { AiFillInstagram } from 'react-icons/ai';
+import { MdMail } from 'react-icons/md';
+import './index.css';
 
 function App() {
   return (
-    <BackgroundGradientAnimation>
-      <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-        <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          Gradients X Animations
-        </p>
+    <div>
+      <h1>Photography Club of the University of Ottawa
+      </h1>
+      <div>
+        <FaDiscord size={80} />
+        <AiFillInstagram size={80} />
+        <MdMail size={80} />
       </div>
-    </BackgroundGradientAnimation>
+    </div>
   );
 }
 
