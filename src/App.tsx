adfaf5ca5660557076c5cd4cf77588a7af661fd5
jwt2706 <jwt2home@gmail.com>
2024-07-31 20:53:42 -0400

    footer works :)

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,31 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="relative min-h-screen overflow-hidden">
+    <div className="relative min-h-screen flex flex-col">
       <Background />
-      <div className="absolute inset-0 z-10 overflow-auto">
-        <div className="mt-4 ml-4 z-30 text-white text-lg md:text-xl">
-          <button onClick={toggleLanguage} className="mr-2">
-            <span className={language === 'en' ? 'underline' : ''}>En</span> / <span className={language === 'fr' ? 'underline' : ''}>Fr</span>
-          </button>
+      <div className="absolute inset-0 z-10 flex flex-col">
+        <div className="flex-grow overflow-auto">
+          <div className="mt-4 ml-4 z-30 text-white text-lg md:text-xl">
+            <button onClick={toggleLanguage} className="mr-2">
+              <span className={language === 'en' ? 'underline' : ''}>En</span> / <span className={language === 'fr' ? 'underline' : ''}>Fr</span>
+            </button>
+          </div>
+          <div className="md:mt-20 flex flex-col items-center justify-center text-black z-20 p-4">
+            <Title language={language} />
+            <Links language={language} />
+            <a
+              href="/Constitution_of_The_Photography_Club_Bilingual.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-10 md:mt-20 text-white underline flex items-center border-2 border-white rounded-lg p-2"
+            >
+              <span className="mr-2">{language === 'en' ? 'Club Constitution' : 'Constitution du Club'}</span>
+              <FiExternalLink size={20} />
+            </a>
+          </div>
         </div>
-        <div className="md:mt-20 flex flex-col items-center justify-center text-black z-20 p-4">
-          <Title language={language} />
-          <Links language={language} />
-          <a
-            href="/Constitution_of_The_Photography_Club_Bilingual.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-10 md:mt-20 text-white underline flex items-center border-2 border-white rounded-lg p-2"
-          >
-            <span className="mr-2">{language === 'en' ? 'Club Constitution' : 'Constitution du Club'}</span>
-            <FiExternalLink size={20} />
-          </a>
-        </div>
-        <footer className="w-full text-white text-center p-4 z-10">
-          <p>&copy; 2021 - {currentYear} uOttawa Photography Club | Club de Photographie d'uOttawa</p>
+        <footer className="w-full text-white text-center p-4 z-10 mt-auto border-t-2 bg-white bg-opacity-20">
+          <p>&copy; 2021 - {currentYear} | {language === 'en' ? 'uOttawa Photography Club' : 'Club de Photographie d\'uOttawa'}</p>
         </footer>
       </div>
     </div>
